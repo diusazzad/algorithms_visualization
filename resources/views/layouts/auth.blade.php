736e8ad224976e8e596eecdb864b53bd00192ef1
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2023-07-13 20:55:04 +0600

    responsive sidenav

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <title>Algorithm Visualization</title>
     @vite('resources/css/app.css')
 </head>
