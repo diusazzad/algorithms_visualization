a129eefb33a4fd41f467a68f0969bf6e8a376839
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2023-07-15 21:45:07 +0600

    update search

--- resources/views/components/user/algorithm-search.blade.php
+++ resources/views/components/user/algorithm-search.blade.php
@@ -1,11 +1,9 @@
 <div class="container mx-auto p-4">
-    <h3 class="text-center">Search Your Algorithm</h3><br />
     <div class="border border-gray-300 rounded p-4">
-        <div class="bg-gray-200 px-4 py-2">Search Data</div>
         <div class="p-4">
-            <div class="mb-4">
+            <div class="mb-1">
                 <input type="text" name="search" id="search" class="border border-gray-300 rounded p-2 w-full"
-                    placeholder="Search Customer Data" />
+                    placeholder="Search Algorithm View" />
             </div>
             <div class="table-responsive">
                 <h3 class="text-center">Total Data: <span id="total_records"></span></h3>
